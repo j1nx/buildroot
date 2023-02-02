b7464dc715187d49543b77cc648b57e62ffc7f54
Christian Stewart <christian@paral.in>
2023-02-02 00:52:25 -0800

    package/go-bootstrap: split into two stages: go1.4 and go1.19.5
    Go 1.20 requires a minimum version of go 1.17.13 to bootstrap.
    
    https://go.dev/doc/go1.20#bootstrap
    
    As Go 1.4 was the previous version that could be compiled with C, there is now
    no way to bootstrap go with a C compiler, unless we use a two-stage bootstrap:
    
     - build host-go-bootstrap-1.4-20170531
     - build host-go-bootstrap-1.19.5 with host-go-bootstrap-1.4-20170531
     - build host-go-1.20 with host-go-bootstrap-1.19.5
    
    This is implemented in this commit first, before upgrading host-go to 1.20.
    
    Note: the .patch files from package/go version 1.19.x are not necessary for
    package/go-bootstrap-stage2 and have not been included there.
    
    Discussion of possible alternate approaches for future use:
    
    https://lore.kernel.org/all/CA+h8R2rtcynkCBsz=_9yANOEguyPCOcQDj8_ns+cv8RS8+8t9A@mail.gmail.com/
    https://lore.kernel.org/all/20220525234312.643dfc03@windsurf/T/
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    
    ---
    
    v1 -> v2:
    
     - fix indentation of GOROOT_BOOTSTRAP in stage2 MAKE_ENV
     - adjust commit description
     - remove duplicate MAKE_ENV declaration line
     - reorder depends on lines in config.in.host
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/go-bootstrap-stage2/go-bootstrap-stage2.mk
+++ package/go-bootstrap-stage2/go-bootstrap-stage2.mk
@@ -4,9 +4,13 @@
 #
 ################################################################################
 
+<<<<<<< HEAD
 # Use last Go version that go-bootstrap-stage1 can build: v1.19.x
 # See https://golang.org/doc/install/source#bootstrapFromSource
 GO_BOOTSTRAP_STAGE2_VERSION = 1.19.11
+=======
+GO_BOOTSTRAP_STAGE2_VERSION = 1.19.5
+>>>>>>> 56066b2b90 (package/go-bootstrap: split into two stages: go1.4 and go1.19.5)
 GO_BOOTSTRAP_STAGE2_SITE = https://storage.googleapis.com/golang
 GO_BOOTSTRAP_STAGE2_SOURCE = go$(GO_BOOTSTRAP_STAGE2_VERSION).src.tar.gz
 
@@ -18,8 +22,13 @@ HOST_GO_BOOTSTRAP_STAGE2_DEPENDENCIES = host-go-bootstrap-stage1
 
 HOST_GO_BOOTSTRAP_STAGE2_ROOT = $(HOST_DIR)/lib/go-$(GO_BOOTSTRAP_STAGE2_VERSION)
 
+<<<<<<< HEAD
 # The go build system is not compatible with ccache, so use
 # HOSTCC_NOCCACHE.  See https://github.com/golang/go/issues/11685.
+=======
+# The go build system is not compatable with ccache, so use HOSTCC_NOCCACHE.
+# See https://github.com/golang/go/issues/11685.
+>>>>>>> 56066b2b90 (package/go-bootstrap: split into two stages: go1.4 and go1.19.5)
 HOST_GO_BOOTSTRAP_STAGE2_MAKE_ENV = \
 	GO111MODULE=off \
 	GOROOT_BOOTSTRAP=$(HOST_GO_BOOTSTRAP_STAGE1_ROOT) \
@@ -46,8 +55,17 @@ define HOST_GO_BOOTSTRAP_STAGE2_INSTALL_CMDS
 	cp -a $(@D)/pkg/include $(@D)/pkg/linux_* $(HOST_GO_BOOTSTRAP_STAGE2_ROOT)/pkg/
 	cp -a $(@D)/pkg/tool $(HOST_GO_BOOTSTRAP_STAGE2_ROOT)/pkg/
 
+<<<<<<< HEAD
 	# The Go sources must be installed to the host/ tree for the Go stdlib.
 	cp -a $(@D)/src $(HOST_GO_BOOTSTRAP_STAGE2_ROOT)/
+=======
+	# https://golang.org/issue/2775
+	cp -a $(@D)/src $(HOST_GO_BOOTSTRAP_STAGE2_ROOT)/
+
+	# Set all file timestamps to prevent the go compiler from rebuilding any
+	# built in packages when programs are built.
+	find $(HOST_GO_BOOTSTRAP_STAGE2_ROOT) -type f -exec touch -r $(@D)/bin/go {} \;
+>>>>>>> 56066b2b90 (package/go-bootstrap: split into two stages: go1.4 and go1.19.5)
 endef
 
 $(eval $(host-generic-package))
