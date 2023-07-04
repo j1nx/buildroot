0d635696b37740bdc8634c53cb8b5d8474ef5e63
j1nx <p.steenbergen@j1nx.nl>
2023-07-04 14:26:02 +0000

    Add podman-compase python package

---
+++ package/python-dotenv/python-dotenv.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-dotenv
+#
+################################################################################
+
+PYTHON_DOTENV_VERSION = 1.0.0
+PYTHON_DOTENV_SITE = https://files.pythonhosted.org/packages/31/06/1ef763af20d0572c032fa22882cfbfb005fba6e7300715a37840858c919e
+PYTHON_DOTENV_SETUP_TYPE = setuptools
+PYTHON_DOTENV_LICENSE = FIXME: please specify the exact BSD version
+PYTHON_DOTENV_LICENSE_FILES = LICENSE docs/license.md
+
+$(eval $(python-package))
