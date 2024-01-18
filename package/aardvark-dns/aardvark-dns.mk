d6fc71801902c6a3b81e479b7072b7410992709a
j1nx <p.steenbergen@j1nx.nl>
2024-01-18 17:47:12 +0000

    First pass on implementing aardvark-dns package

---
+++ package/aardvark-dns/aardvark-dns.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# aardvark-dns.mk
+#
+################################################################################
+
+AARDVARK_DNS_VERSION = 1.9.0
+AARDVARK_DNS_SITE = $(call github,containers,aardvark-dns,v$(AARDVARK_DNS_VERSION))
+AARDVARK_DNS_DEPENDENCIES = host-rustc host-pkgconf
+
+AARDVARK_DNS_CARGO_ENV = PKG_CONFIG_ALLOW_CROSS=1
+
+define AARDVARK_DNS_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/target/$(RUSTC_TARGET_NAME)/release/aardvark-dns \
+		$(TARGET_DIR)/usr/bin/aardvark-dns
+endef
+
+$(eval $(cargo-package))
