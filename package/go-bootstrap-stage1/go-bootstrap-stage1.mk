96649229e135cdf15ddb9dc17e72bceb09a04fe7
j1nx <p.steenbergen@j1nx.nl>
2024-03-01 07:50:59 +0000

    Fix merge mistakes and use podman v4.9.3 patched instead of dev sha

--- package/go-bootstrap-stage1/go-bootstrap-stage1.mk
+++ package/go-bootstrap-stage1/go-bootstrap-stage1.mk
@@ -13,20 +13,10 @@ GO_BOOTSTRAP_STAGE1_SOURCE = go$(GO_BOOTSTRAP_STAGE1_VERSION).tar.gz
 GO_BOOTSTRAP_STAGE1_LICENSE = BSD-3-Clause
 GO_BOOTSTRAP_STAGE1_LICENSE_FILES = LICENSE
 
-<<<<<<< HEAD
 HOST_GO_BOOTSTRAP_STAGE1_ROOT = $(HOST_DIR)/lib/go-$(GO_BOOTSTRAP_STAGE1_VERSION)
 
 # The go build system is not compatible with ccache, so use
 # HOSTCC_NOCCACHE. See https://github.com/golang/go/issues/11685.
-=======
-# The toolchain is needed for HOSTCC_NOCACHE used to compile the Go compiler.
-HOST_GO_BOOTSTRAP_STAGE1_DEPENDENCIES = toolchain
-
-HOST_GO_BOOTSTRAP_STAGE1_ROOT = $(HOST_DIR)/lib/go-$(GO_BOOTSTRAP_STAGE1_VERSION)
-
-# The go build system is not compatable with ccache, so use HOSTCC_NOCCACHE.
-# See https://github.com/golang/go/issues/11685.
->>>>>>> 56066b2b90 (package/go-bootstrap: split into two stages: go1.4 and go1.19.5)
 HOST_GO_BOOTSTRAP_STAGE1_MAKE_ENV = \
 	GOOS=linux \
 	GOROOT_FINAL="$(HOST_GO_BOOTSTRAP_STAGE1_ROOT)" \
@@ -46,11 +36,7 @@ define HOST_GO_BOOTSTRAP_STAGE1_INSTALL_CMDS
 	cp -a $(@D)/lib $(HOST_GO_BOOTSTRAP_STAGE1_ROOT)/
 	cp -a $(@D)/pkg $(HOST_GO_BOOTSTRAP_STAGE1_ROOT)/
 
-<<<<<<< HEAD
 	# The Go sources must be installed to the host/ tree for the Go stdlib.
-=======
-	# https://golang.org/issue/2775
->>>>>>> 56066b2b90 (package/go-bootstrap: split into two stages: go1.4 and go1.19.5)
 	cp -a $(@D)/src $(HOST_GO_BOOTSTRAP_STAGE1_ROOT)/
 endef
 
