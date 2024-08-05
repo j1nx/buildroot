afbbc1095dc4c698774686cd17f03a20a0cf63c2
j1nx <p.steenbergen@j1nx.nl>
2024-08-05 08:55:20 +0000

    Bump rpi-firmware

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 8d5579c475569d1cacf6f386122c050ab09e69b0
+RPI_FIRMWARE_VERSION = 571e78a2129578784e81a4312468bb92cc8d623d
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
