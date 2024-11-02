7f4b8bca345f2c7eaaf2d78703a212762cdea37d
j1nx <p.steenbergen@j1nx.nl>
2024-11-02 08:31:02 +0000

    package/embiggen-disk: use findmnt to look up mounts
    Switch to the skiffos fork which has additional features:
    
     - Try to use findmnt to lookup mountpoint information.
     - If findmnt fails, fallback to the older approach.
     - Add no-resize-partition and ignore-resize-partition flags.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/embiggen-disk/embiggen-disk.mk
+++ package/embiggen-disk/embiggen-disk.mk
@@ -9,4 +9,6 @@ EMBIGGEN_DISK_SITE = $(call github,skiffos,embiggen-disk,$(EMBIGGEN_DISK_VERSION
 EMBIGGEN_DISK_LICENSE = Apache-2.0
 EMBIGGEN_DISK_LICENSE_FILES = LICENSE
 
+EMBIGGEN_DISK_GOMOD = github.com/bradfitz/embiggen-disk
+
 $(eval $(golang-package))
